d87b850330d7737098e3c39fd1e4a40ffc08c745
Danrley Silva <danrley.silva@mzgroup.com>
2019-09-16 11:46:43 -0300

    MOD4 - github API

--- modulo-4/exec-2/main.js
+++ modulo-4/exec-2/main.js
@@ -1,24 +1,42 @@
 var elementInput = document.querySelector('#app #user');
 var btnElement = document.querySelector('#app button');
 var listElement = document.querySelector('#app ul');
+var mainElement = document.querySelector('#profile');
+
+function renderImage() {
+    
+    var xhr = new XMLHttpRequest();
+
+    var user = elementInput.value;
+
+    xhr.open('GET', 'https://api.github.com/users/'+user);
+    xhr.send(null);
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState === 4) {
+            var results = (JSON.parse(xhr.responseText));
+            var createImg = document.createElement('img');
+
+            createImg.setAttribute('src', results.avatar_url);
+            mainElement.prepend(createImg); 
+        }
+    }
+    renderList();
+}
 
 function renderList() {
     var xhr = new XMLHttpRequest();
 
     var user = elementInput.value;
 
-    xhr.open('GET', 'https://api.github.com/users/'+user+'/repos');
+    xhr.open('GET', 'https://api.github.com/users/'+user+"/repos");
     xhr.send(null);
 
     xhr.onreadystatechange = function() {
          if (xhr.readyState === 4) {
             var results = (JSON.parse(xhr.responseText));
             console.log(results);
-            var createDiv = document.createElement('div');
-
-            createDiv.style.height = "100px";
-            createDiv.style.width = "100px";
-            createDiv.style.backgroundColor = "#f0f";
+           
             for (listResults of results) {
                 var createElementList = document.createElement('li');
                 var createListText = document.createTextNode(listResults.name);
@@ -35,4 +53,4 @@ function renderList() {
     }
 }
 
-btnElement.onclick = renderList;
+btnElement.onclick = renderImage;
